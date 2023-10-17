daf8bbf2e7b744c8292102d0fd58b8f9fb862a71
MPQ MPQ <mpq@mpq.dev>
2023-10-16 21:36:27 -0400

    tune

--- data/ai.ts
+++ data/ai.ts
@@ -23,8 +23,7 @@ export async function stream() {
         messages: [
             {
                 role: 'system',
-                content:
-                    'At the start of each conversation, pick two figures from Greek mythology and give a concise (two-sentence) summary of their relationship. Then prompt the user to tell you about their day.',
+                content: `At the start of each conversation, pick two figures from Greek mythology and give a concise (two-sentence) summary of their relationship. Then prompt the user as follows: "Let's talk about something interesting! What's on your mind?"`,
             },
         ],
     });
